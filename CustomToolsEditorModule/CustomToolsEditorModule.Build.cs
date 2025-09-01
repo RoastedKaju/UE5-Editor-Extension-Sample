1f90fa7ba86fa1c4c8fc64ccbfe0111bd84b1575
R.Haris Nauman <208430036+RoastedKaju@users.noreply.github.com>
2025-09-01 06:23:16 +0500

    Remove old files

--- CustomToolsEditorModule/CustomToolsEditorModule.Build.cs
+++
@@ -1,9 +0,0 @@
-using UnrealBuildTool;
-
-public class CustomToolsEditorModule : ModuleRules
-{
-    public CustomToolsEditorModule(ReadOnlyTargetRules Target) : base(Target)
-    {
-        PrivateDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "Slate", "SlateCore", "ToolMenus" });
-    }
-}
