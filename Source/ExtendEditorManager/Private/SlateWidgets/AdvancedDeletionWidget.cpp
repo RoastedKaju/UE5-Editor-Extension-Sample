b1a5f58196b4121600c041363b47bb059a0c17bd
R.Haris Nauman <208430036+RoastedKaju@users.noreply.github.com>
2025-09-08 04:06:58 +0500

    Create AdvancedDeletion Compound widget

---
+++ Source/ExtendEditorManager/Private/SlateWidgets/AdvancedDeletionWidget.cpp
@@ -0,0 +1,9 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "SlateWidgets/AdvancedDeletionWidget.h"
+
+void SAdvancedDeletionWidget::Construct(const FArguments& args)
+{
+	bCanSupportFocus = true;
+}
---
+++ Source/ExtendEditorManager/Public/SlateWidgets/AdvancedDeletionWidget.h
@@ -0,0 +1,18 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Widgets/SCompoundWidget.h"
+
+/**
+ * 
+ */
+class EXTENDEDITORMANAGER_API SAdvancedDeletionWidget : public SCompoundWidget
+{
+	SLATE_BEGIN_ARGS(SAdvancedDeletionWidget) {}
+	SLATE_END_ARGS()
+
+public:
+	void Construct(const FArguments& args);
+};
