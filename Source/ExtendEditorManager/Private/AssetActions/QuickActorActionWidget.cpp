47a68062bc1ff7dccb53886bcab587fc7f4ce534
R.Haris Nauman <208430036+RoastedKaju@users.noreply.github.com>
2025-10-09 00:24:27 +0500

    Add batch actor selection

---
+++ Source/ExtendEditorManager/Private/AssetActions/QuickActorActionWidget.cpp
@@ -0,0 +1,67 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "AssetActions/QuickActorActionWidget.h"
+#include "Subsystems/EditorActorSubsystem.h"
+#include "DebugHeader.h"
+
+bool UQuickActorActionWidget::GetEditorActorSubsystem()
+{
+	if (!EditorActorSubsystem)
+	{
+		EditorActorSubsystem = GEditor->GetEditorSubsystem<UEditorActorSubsystem>();
+	}
+
+	return EditorActorSubsystem != nullptr;
+}
+
+void UQuickActorActionWidget::SelectAllActorsWithSimilarName()
+{
+	if (!GetEditorActorSubsystem())
+	{
+		return;
+	}
+
+	TArray<AActor*> SelectedActors = EditorActorSubsystem->GetSelectedLevelActors();
+	uint32 SelectionCounter = 0;
+
+	if (SelectedActors.IsEmpty())
+	{
+		DebugHelper::ShowNotification(TEXT("No Actors Selected"));
+		return;
+	}
+
+	if (SelectedActors.Num() > 1)
+	{
+		DebugHelper::ShowNotification(TEXT("You can only select one actor."));
+		return;
+	}
+	
+	FString SelectedActorLabel = SelectedActors[0]->GetActorLabel();
+	const FString LabelToSearch = SelectedActorLabel.LeftChop(4);
+
+	TArray<AActor*> AllLevelActors = EditorActorSubsystem->GetAllLevelActors();
+
+	for (const auto& LevelActor : AllLevelActors)
+	{
+		if (!IsValid(LevelActor))
+		{
+			continue;
+		}
+
+		if (LevelActor->GetActorLabel().Contains(LabelToSearch, SearchCase))
+		{
+			EditorActorSubsystem->SetActorSelectionState(LevelActor, true);
+			SelectionCounter++;
+		}
+	}
+
+	if (SelectionCounter > 0)
+	{
+		DebugHelper::ShowNotification(TEXT("Successfully selected ") + FString::FromInt(SelectionCounter) + TEXT(" actors"));
+	}
+	else
+	{
+		DebugHelper::ShowNotification(TEXT("No actors selected"));
+	}
+}
---
+++ Source/ExtendEditorManager/Public/AssetActions/QuickActorActionWidget.h
@@ -0,0 +1,31 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "EditorUtilityWidget.h"
+#include "QuickActorActionWidget.generated.h"
+
+class UEditorActorSubsystem;
+
+/**
+ * 
+ */
+UCLASS()
+class EXTENDEDITORMANAGER_API UQuickActorActionWidget : public UEditorUtilityWidget
+{
+	GENERATED_BODY()
+
+public:
+	bool GetEditorActorSubsystem();
+
+	UFUNCTION(BlueprintCallable)
+	void SelectAllActorsWithSimilarName();
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "ActorBatchSelection")
+	TEnumAsByte<ESearchCase::Type> SearchCase = ESearchCase::IgnoreCase;
+
+private:
+UPROPERTY()
+	UEditorActorSubsystem* EditorActorSubsystem;
+};
