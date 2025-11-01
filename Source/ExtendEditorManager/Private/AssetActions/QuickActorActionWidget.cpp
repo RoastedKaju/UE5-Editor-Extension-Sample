9f1939c02662d974ba6660efa03a51a5bc04ffe6
R.Haris Nauman <208430036+RoastedKaju@users.noreply.github.com>
2025-11-01 12:44:02 +0500

    Add actor duplication function

--- Source/ExtendEditorManager/Private/AssetActions/QuickActorActionWidget.cpp
+++ Source/ExtendEditorManager/Private/AssetActions/QuickActorActionWidget.cpp
@@ -36,7 +36,7 @@ void UQuickActorActionWidget::SelectAllActorsWithSimilarName()
 		DebugHelper::ShowNotification(TEXT("You can only select one actor."));
 		return;
 	}
-	
+
 	FString SelectedActorLabel = SelectedActors[0]->GetActorLabel();
 	const FString LabelToSearch = SelectedActorLabel.LeftChop(4);
 
@@ -63,5 +63,69 @@ void UQuickActorActionWidget::SelectAllActorsWithSimilarName()
 	else
 	{
 		DebugHelper::ShowNotification(TEXT("No actors selected"));
+	}
+}
+
+void UQuickActorActionWidget::DuplicateActors()
+{
+	if (!GetEditorActorSubsystem())
+	{
+		return;
+	}
+
+	TArray<AActor*> SelectedActors = EditorActorSubsystem->GetSelectedLevelActors();
+
+	if (SelectedActors.IsEmpty())
+	{
+		DebugHelper::ShowNotification(TEXT("No Actors Selected"));
+		return;
+	}
+
+	if (OffsetDistance == 0 || NumberOfDuplicates <= 0)
+	{
+		DebugHelper::ShowNotification(TEXT("Offset Distance or Duplicate Number is invalid."));
+		return;
+	}
+
+	int32 Counter = 0;
+
+	for (AActor* SelectedActor : SelectedActors)
+	{
+		if (!SelectedActor) continue;
+
+		for (int32 i = 0; i < NumberOfDuplicates; i++)
+		{
+			AActor* DuplicatedActor = EditorActorSubsystem->DuplicateActor(SelectedActor, SelectedActor->GetWorld());
+
+			if (!DuplicatedActor) continue;
+
+			const float DuplicationOffsetDist = (i + 1) * OffsetDistance;
+
+			switch (DuplicationAxis)
+			{
+			case EDuplicationAxis::XAxis:
+				DuplicatedActor->AddActorWorldOffset(FVector(DuplicationOffsetDist, 0.0f, 0.0f));
+				break;
+			case EDuplicationAxis::YAxis:
+				DuplicatedActor->AddActorWorldOffset(FVector(0.0f, DuplicationOffsetDist, 0.0f));
+				break;
+			case EDuplicationAxis::ZAxis:
+				DuplicatedActor->AddActorWorldOffset(FVector(0.0f, 0.0f, DuplicationOffsetDist));
+				break;
+			case EDuplicationAxis::Max:
+				break;
+
+			default:
+				break;
+			}
+
+			EditorActorSubsystem->SetActorSelectionState(DuplicatedActor, true);
+			Counter++;
+		}
+	}
+
+	if (Counter > 0)
+	{
+		DebugHelper::ShowNotification(TEXT("Successfully Duplicated ") + FString::FromInt(Counter) + TEXT(" Actors"));
 	}
 }
--- Source/ExtendEditorManager/Public/AssetActions/QuickActorActionWidget.h
+++ Source/ExtendEditorManager/Public/AssetActions/QuickActorActionWidget.h
@@ -8,6 +8,15 @@
 
 class UEditorActorSubsystem;
 
+UENUM(BlueprintType)
+enum class EDuplicationAxis : uint8
+{
+	XAxis		UMETA(DisplayName = "X-Axis"),
+	YAxis		UMETA(DisplayName = "Y-Axis"),
+	ZAxis		UMETA(DisplayName = "Z-Axis"),
+	Max			UMETA(DisplayName = "Max")
+};
+
 /**
  * 
  */
@@ -24,6 +33,18 @@ public:
 
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "ActorBatchSelection")
 	TEnumAsByte<ESearchCase::Type> SearchCase = ESearchCase::IgnoreCase;
+
+	UFUNCTION(BlueprintCallable)
+	void DuplicateActors();
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "ActorDuplication")
+	EDuplicationAxis DuplicationAxis = EDuplicationAxis::XAxis;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "ActorDuplication")
+	float OffsetDistance = 300.0f;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "ActorDuplication")
+	int32 NumberOfDuplicates = 3;	
 
 private:
 UPROPERTY()
